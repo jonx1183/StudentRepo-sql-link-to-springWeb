48e55d7e6884d75d9703495ee5f40d0c75f60ed1
jonx1183 <jonx1183@stud.kea.dk>
2022-10-14 11:09:51 +0200

    Now able to get to page where you can type in name, email, and gender Can't add to Student1 database?

--- src/main/java/com/example/p13102022/Controller/HomeController.java
+++ src/main/java/com/example/p13102022/Controller/HomeController.java
@@ -1,4 +1,20 @@
 package com.example.p13102022.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
+
+@Controller
 public class HomeController {
+  @GetMapping("/create")
+  public String index(){
+    return "index";
+  }
+
+  @PostMapping("/create")
+  public String create(WebRequest payload){
+    return "redirect:/";
+
+  }
 }
